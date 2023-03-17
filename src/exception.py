e4cf6d6ae45be3c7aae9587538a163e55f20bb64
bele99 <liubinbin8899@gmail.com>
2023-03-17 22:04:08 +1100

    data proprocessing
    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,12 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python script name [0] line number[{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
-    )
+    error_message = "Error occured in python script name [{0}] line number[{1}] error message[{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
@@ -22,7 +22,7 @@ class CustomException(Exception):
 #    try:
 #        a=1/0
 #    except Exception as e:
-#        #logging.info("Divide by Zero")
+#        logging.info("Divide by Zero")
 #        raise CustomException(e, sys)
         
     
